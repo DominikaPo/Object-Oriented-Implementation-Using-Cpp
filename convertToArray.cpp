b821f9758f6afddb7f477d82797bf0881d2c210e
DominikaPo <116868602+DominikaPo@users.noreply.github.com>
2023-05-11 20:42:42 +0200

    Add files via upload

---
+++ convertToArray.cpp
@@ -0,0 +1,35 @@
+/*Write a C++ program to find the character for a given index position in the string.*/
+
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main() //DO NOT change the 'main' signature
+{
+    //Call the function and print the result
+    
+    string name = "";
+    int index;
+    int len;
+    
+    cout<<"Enter the name: ";
+    cin>>name;
+    
+    cout<<"Enter the index: ";
+    cin>>index;
+    
+    char arr[name.length() + 1];
+    
+    strcpy(arr, name.c_str());
+    
+    len = name.length();
+    
+    if (index < len) {
+       cout<<"The character is "<<arr[index];  
+    }
+    else {
+        cout<<"error";
+    }
+    
+    return 0;
+}
